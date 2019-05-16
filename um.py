308ddcbe2da2aa21a1cd2f0ddf01b03a881312aa
Nicholas-ar <njaf@ic.ufal.br>
2019-05-15 22:39:09 -0300

    changed variable names for clarity and added output on terminal and file

--- um.py
+++ um.py
@@ -2,20 +2,33 @@ import re
 import operator
 
 # abre o arquivo, verifica se abriu corretamente, se sim, copia o valor para a var content
-#filename = 'corpus-sentence-pt-br.txt'
-filename = 'a.txt'
-f = open(filename, 'r')
-if f.mode == 'r':
-    content = f.read()
-f.close()
+input_filename = 'entrada.txt'
+arquivo_entrada = open(input_filename, 'r')
+if arquivo_entrada.mode == 'r':
+    content = arquivo_entrada.read()
+arquivo_entrada.close()
 
 # deixa todas as palavras em caixa baixa
 content = content.lower()
 
-#separa todas as palavras e pontuação, palavras com hífen são consideradas uma única palavra
+# separa todas as palavras e pontuação, palavras com hífen são consideradas uma única palavra
 lista = re.findall(r'[\w-]+[\w]+|[.,!?;]', content)
 
-#cria um dicionário onde a chave é a palavra, e o valor a quantidade de vezes que aparece
+# cria um dicionário onde a chave é a palavra, e o valor a quantidade de vezes que aparece
 resultados = {}
 for item in lista:
     resultados[item] = lista.count(item)
+
+# cria uma lista ordenada de forma decrescente de acordo com a frequência que aparece
+decrescente = sorted(resultados.items(),
+                     key=operator.itemgetter(1), reverse=True)
+
+# define o arquivo para a saída
+output_filename = 'saida.txt'
+arquivo_saida = open(output_filename, 'w')
+
+# percorre a lista ordenada para adicionar ao terminal e ao arquivo de saída
+for item in decrescente:
+    print(item[0])
+    arquivo_saida.write('%s\n' % item[0])
+arquivo_saida.close()
